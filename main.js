177247e53d94bb78b0ff3c343fb8a1f7a22ea7cc
Jared Rigby <talk@jaredrigby.co.uk>
2023-11-02 20:45:45 +0800

    Created placeholder webpage

---
+++ main.js
@@ -0,0 +1,16 @@
+const body = document.getElementById("js-background-wrapper");
+
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+body.onmousemove = e => handleOnMouseMove(e);
+
+console.log("You're an inquisitive one aren't you? The Father is very keen to speak with you.");
